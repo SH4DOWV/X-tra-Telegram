27d594dbf62498dbdabcc7646601b97ddda12e40
SH4DOWV <34161328+SH4DOWV@users.noreply.github.com>
2020-05-10 22:09:47 +0200

    Update meme.py

--- userbot/plugins/meme.py
+++ userbot/plugins/meme.py
@@ -1,13 +1,15 @@
 """
 Memes Plugin for Userbot
 usage = .meme someCharacter //default delay will be 3
-By : - @Zero_cool7870
+
 
 """
 from telethon import events
 import asyncio
 import os
 import sys
+from userbot.events import register, errors_handler
+from userbot import CMD_HELP
 
 
 @borg.on(events.NewMessage(pattern=r"\.meme", outgoing=True))
@@ -58,4 +60,116 @@ async def meme(event):
     await event.edit(flower+flower+flower+flower+flower+flower+flower+flower+flower+flower)
     await asyncio.sleep(sleepValue)
         
-    
+
+   @register(pattern='.type(?: |$)(.*)')
+@errors_handler
+async def typewriter(typew):
+    """ Just a small command to make your keyboard become a typewriter! """
+    if not typew.text[0].isalpha() and typew.text[0] not in ("/", "#", "@",
+                                                             "!"):
+        textx = await typew.get_reply_message()
+        message = typew.pattern_match.group(1)
+        if message:
+            pass
+        elif textx:
+            message = textx.text
+        else:
+            await typew.edit("`Give a text to type!`")
+            return
+        sleep_time = 0.03
+        typing_symbol = "|"
+        old_text = ''
+        await typew.edit(typing_symbol)
+        await asyncio.sleep(sleep_time)
+        for character in message:
+            old_text = old_text + "" + character
+            typing_text = old_text + "" + typing_symbol
+            await typew.edit(typing_text)
+            await asyncio.sleep(sleep_time)
+            await typew.edit(old_text)
+            await asyncio.sleep(sleep_time)
+
+@register(outgoing=True, pattern="^.bt$")
+@errors_handler
+async def bluetext(bt_e):
+    """ Believe me, you will find this useful. """
+    if not bt_e.text[0].isalpha() and bt_e.text[0] not in ("/", "#", "@", "!"):
+        if await bt_e.get_reply_message():
+            await bt_e.edit(
+                "`TESTO BLU, BASTA CLICCARE.`\n"
+                "`Sei uno stupido animale attratto dai colori?`")
+ 
+@register(outgoing=True, pattern="^.bm(?: |$)(.*)")
+@errors_handler
+async def spongemocktext(mock):
+    """ Do it and find the real fun. """
+    if not mock.text[0].isalpha() and mock.text[0] not in ("/", "#", "@", "!"):
+        reply_text = list()
+        textx = await mock.get_reply_message()
+        message = mock.pattern_match.group(1)
+        if message:
+            pass
+        elif textx:
+            message = textx.text
+        else:
+            await mock.edit("`gIvE sOMEtHInG tO MoCk!`")
+            return
+
+        for charac in message:
+            if charac.isalpha() and random.randint(0, 1):
+                to_app = charac.upper() if charac.islower() else charac.lower()
+                reply_text.append(to_app)
+            else:
+                reply_text.append(charac)
+
+        await mock.edit("".join(reply_text))
+
+
+@register(outgoing=True, pattern="^.str(?: |$)(.*)")
+@errors_handler
+async def stretch(stret):
+    """ Stretch it."""
+    if not stret.text[0].isalpha() and stret.text[0] not in ("/", "#", "@",
+                                                             "!"):
+        textx = await stret.get_reply_message()
+        message = stret.text
+        message = stret.pattern_match.group(1)
+        if message:
+            pass
+        elif textx:
+            message = textx.text
+        else:
+            await stret.edit("`GiiiiiiiB sooooooomeeeeeee teeeeeeext!`")
+            return
+
+        count = random.randint(3, 10)
+        reply_text = re.sub(r"([aeiouAEIOUａｅｉｏｕＡＥＩＯＵаеиоуюяыэё])",
+                            (r"\1" * count), message)
+        await stret.edit(reply_text)
+
+
+@register(outgoing=True, pattern="^.vpr(?: |$)(.*)")
+@errors_handler
+async def vapor(vpr):
+    """ Vaporize everything! """
+    if not vpr.text[0].isalpha() and vpr.text[0] not in ("/", "#", "@", "!"):
+        reply_text = list()
+        textx = await vpr.get_reply_message()
+        message = vpr.pattern_match.group(1)
+        if message:
+            pass
+        elif textx:
+            message = textx.text
+        else:
+            await vpr.edit("`Ｇｉｖｅ ｓｏｍｅ ｔｅｘｔ ｆｏｒ ｖａｐｏｒ！`")
+            return
+
+        for charac in message:
+            if 0x21 <= ord(charac) <= 0x7F:
+                reply_text.append(chr(ord(charac) + 0xFEE0))
+            elif ord(charac) == 0x20:
+                reply_text.append(chr(0x3000))
+            else:
+                reply_text.append(charac)
+
+        await vpr.edit("".join(reply_text))
