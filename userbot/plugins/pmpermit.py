e7182846b91290c20b2dcd4083c29c0929828ce4
SH4DOWV <34161328+SH4DOWV@users.noreply.github.com>
2020-05-12 13:26:05 +0200

    Update pmpermit.py

--- userbot/plugins/pmpermit.py
+++ userbot/plugins/pmpermit.py
@@ -83,9 +83,10 @@ if Var.PRIVATE_GROUP_ID is not None:
         if len(approved_users) > 0:
             for a_user in approved_users:
                 if a_user.reason:
-                    APPROVED_PMs += f"👉 [{a_user.chat_id}](tg://user?id={}).format(chat.id) per {a_user.reason}\n"
+                    APPROVED_PMs += f"👉 [{a_user.chat_id}](tg://user?id={})".format(chat.id)\n
+                                    f"per {a_user.reason}"
                 else:
-                    APPROVED_PMs += f"👉 [{a_user.chat_id}](tg://user?id={})\n".format(chat.id)
+                    APPROVED_PMs += f"👉 [{a_user.chat_id}](tg://user?id={})".format(chat.id)
         else:
             APPROVED_PMs = "Nessuno autorizzato, per ora"
         if len(APPROVED_PMs) > 4095:
