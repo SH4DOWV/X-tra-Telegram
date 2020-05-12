459668d05f984468914adc04ce5877a868d765f6
SH4DOWV <34161328+SH4DOWV@users.noreply.github.com>
2020-05-12 21:51:52 +0200

    Update Shadow.py

--- userbot/plugins/Shadow.py
+++ userbot/plugins/Shadow.py
@@ -7,7 +7,7 @@ from userbot.utils import admin_cmd
 @command(outgoing=True, pattern="^.shadowid$")
 async def amireallyalive(shadow):
     """ For .alive command, check if the bot is running.  """
-    await shadow.edit("**📖Ecco i miei ID:\n\n"
+    await shadow.edit("**📖Ecco i miei ID**:\n\n"
                      "**📄Telegram:** @V_SHADOW_V\n\n"
                      "**💻PSN: Shadowz_SWAT**\n\n"
                      "**💣COD: Shadow#4654236**")
