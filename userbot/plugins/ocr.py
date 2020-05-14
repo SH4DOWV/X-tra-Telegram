288d9cb87c2c8ca50a43b6d70ec7ddc3bea6dcb6
SH4DOWV <34161328+SH4DOWV@users.noreply.github.com>
2020-05-14 20:36:57 +0200

    Update ocr.py

--- userbot/plugins/ocr.py
+++ userbot/plugins/ocr.py
@@ -44,7 +44,7 @@ async def ocr_space_file(filename,
 
 @register(pattern="^.ocr(?: |$)(.*)", outgoing=True)
 async def ocr(event):
-    await event.edit("`Reading...`")
+    await event.edit("`👀Leggendo...`")
     if not os.path.isdir(TEMP_DOWNLOAD_DIRECTORY):
         os.makedirs(TEMP_DOWNLOAD_DIRECTORY)
     lang_code = event.pattern_match.group(1)
@@ -55,9 +55,9 @@ async def ocr(event):
     try:
         ParsedText = test_file["ParsedResults"][0]["ParsedText"]
     except BaseException:
-        await event.edit("`Couldn't read it.`\n`I guess I need new glasses.`")
+        await event.edit("**Non riesco a leggerlo.**\n**Mi servono degli occhiali👓.**")
     else:
-        await event.edit(f"`Here's what I could read from it:`\n\n{ParsedText}"
+        await event.edit(f"**📖Ecco cosa ho letto:**\n\n{ParsedText}"
                          )
     os.remove(downloaded_file_name)
 
