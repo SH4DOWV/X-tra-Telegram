cd255463c4b147ebe3b6a8690dc7b0bad848d146
SH4DOWV <34161328+SH4DOWV@users.noreply.github.com>
2020-05-16 19:32:47 +0200

    Update and rename mf.py to Dc.py

---
+++ userbot/plugins/Dc.py
@@ -0,0 +1,12 @@
+import sys
+from telethon import events, functions, __version__
+from userbot.utils import admin_cmd
+
+
+@borg.on(admin_cmd(pattern="dc"))  # pylint:disable=E0602
+async def _(event):
+    if event.fwd_from:
+        return
+    result = await borg(functions.help.GetNearestDcRequest())  # pylint:disable=E0602
+    await event.edit(result.stringify())
+
