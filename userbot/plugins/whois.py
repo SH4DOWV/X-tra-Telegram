02c8e1a39404a097c6cad2588de5900724aad2ce
SH4DOWV <34161328+SH4DOWV@users.noreply.github.com>
2020-05-26 22:25:04 +0200

    Update whois.py

--- userbot/plugins/whois.py
+++ userbot/plugins/whois.py
@@ -46,7 +46,7 @@ async def _(event):
     except Exception as e:
         dc_id = "**Serve una foto profilo per vedere il DataCenter.**"
         location = str(e)
-    caption = """<b>Informazioni dell'utente estratte da</b> [𝙎𝙝𝙖𝙙𝙤𝙬](tg://user?id=309290540)\n
+    caption = """<b>Informazioni dell'utente estratte da</b> 𝙎𝙝𝙖𝙙𝙤𝙬\n
 <b>• 🆔Utente:</b> <code>{}</code>
 <b>• 👤Profilo:</b> <a href='tg://user?id={}'>{}</a>
 <b>• ⌨️Bio:</b> {}
