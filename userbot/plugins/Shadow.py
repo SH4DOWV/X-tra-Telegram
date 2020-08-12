73634556989ac274c44a0a2cc9ff4322e7a52158
SH4DOWV <34161328+SH4DOWV@users.noreply.github.com>
2020-08-12 11:20:42 +0200

    Update Shadow.py

--- userbot/plugins/Shadow.py
+++ userbot/plugins/Shadow.py
@@ -9,7 +9,7 @@ async def amireallyalive(shadow):
     """ For .alive command, check if the bot is running.  """
     await shadow.edit("**📖Ecco i miei ID**:\n\n"
                      "**📄Telegram:** @V_SHADOW_V\n\n"
-                     "**💻PSN: Shadowz_SWAT**\n\n"
+                     "**Epic: V_SH4DOW_V**\n\n"
                      "**💣COD: Shadow#4654236**")
                      # Don't change this else you a TikTok loser, Son of Jinping. Add your own.
                      
